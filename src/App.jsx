dcce9c72b560044cdafe7270d443a0a4be3eb39f
Tinayeshe Mugwagwa <tinayeshem@gmail.com>
2024-08-05 22:55:53 +0200

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Card from './components/Card'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar'
 
 
         
@@ -12,8 +14,33 @@ import Card from './components/Card'
 function App() {  
 
       return (
-      <> 
-         <Card/> 
+      
+            <>
+            <Navbar/> 
+            <div className="container">
+               <div className="row">
+                  <div className="col-sm">
+                     <Card/> 
+                     <Card/> 
+                     <Card/>
+                  </div>
+                  <div className="col-sm">
+                     <Card/> 
+                     <Card/> 
+                     <Card/>
+                  </div>
+                  <div className="col-sm">
+                     <Card/>
+                     <Card/>
+                     <Card/>
+                  </div>
+               
+               
+               </div>
+                
+                
+                               
+            </div>
 
 
       </>    
